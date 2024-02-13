f41ed31e54da931494f598f202365e35fa8574c6
thowfeeq-mohamed123 <mohameddowbi6@gmail.com>
2024-02-13 15:29:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const serverless = require("serverless-http");
+
+app.get("/", (req, res) => {
+  res.send({ status: 200, message: "Hello world!" });
+});
+
+const handler = serverless(app);
+
+const startServer = async () => {
+  app.listen(3000, () => {
+    console.log("listening on port 3000!");
+  });
+};
+
+startServer();
+
+module.exports.handler = (event, context, callback) => {
+  const response = handler(event, context, callback);
+  return response;
+};
